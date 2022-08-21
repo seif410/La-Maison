d9ed7edc13ad674670058fdd232d1753e4937d0e
seif amr <seifamr410@gmail.com>
2022-08-21 21:09:40 +0200

    some modifications

--- app/src/main/java/com/example/lamaison/MyAdapter.java
+++ app/src/main/java/com/example/lamaison/MyAdapter.java
@@ -1,5 +1,6 @@
 package com.example.lamaison;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private Context context;
     private ArrayList item_name, item_price;
+
 
     public MyAdapter(Context context, ArrayList item_name, ArrayList item_price) {
         this.context = context;
@@ -31,9 +34,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.item_name.setText(String.valueOf(item_name.get(position)));
         holder.item_price.setText(String.valueOf(item_price.get(position)));
+        holder.deletebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, String.valueOf(item_price.get(position)), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -50,7 +59,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             item_name = itemView.findViewById(R.id.itemsname);
             item_price = itemView.findViewById(R.id.itemsprice);
             deletebtn = itemView.findViewById(R.id.deletebtn);
-
         }
+
+
     }
+
 }
